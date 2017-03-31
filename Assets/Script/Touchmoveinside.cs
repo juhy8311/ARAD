41d4d9d3eb10c74921786173ff0f058c4821ca6a
juhy8311@naver.com <juhy8311@naver.com>
2017-03-31 20:01:58 +0900

    inside 360camera add

---
+++ Assets/Script/Touchmoveinside.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touchmoveinside : MonoBehaviour {
+
+    private Touch initTouch = new Touch();
+    public Camera cam;
+
+    private float rotX = 0f;
+    private float rotY = 0f;
+    private Vector3 origRot;
+
+    public float rotSpeed = 0.5f;
+    public float dir = -1;
+
+	// Use this for initialization
+	void Start () {
+
+        origRot = cam.transform.eulerAngles;
+        rotX = origRot.x;
+        rotY = origRot.y;
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                initTouch = touch;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                //swiping action
+                float deltaX = initTouch.position.x - touch.position.x;
+                float deltaY = initTouch.position.y - touch.position.y;
+                rotX -= deltaY * Time.deltaTime * rotSpeed * dir;
+                rotY += deltaX * Time.deltaTime * rotSpeed * dir;
+                rotX = Mathf.Clamp(rotX, -45f,45f);
+
+
+                cam.transform.eulerAngles = new Vector3(rotX, rotY, 0f);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                initTouch = new Touch();
+            }
+        }
+
+    }
+}
